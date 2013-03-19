87954dca73a902687c48e8fa71b62982ef4ac18c
leeper48 <kjordan@pmg.com>
2013-03-19 13:36:23 -0400

    Add page controls

---
+++ test.rb
@@ -0,0 +1,22 @@
+# class Family
+  
+def fammember(first,last)
+  person = first, last
+end
+  
+
+# person1 = {first: "Kurt", last: "Jordan"}
+# person2 = {first: "Amy", last: "Jordan"}
+# person3 = {first: "Kat", last: "Jordan"}
+# 
+# params = {}
+# 
+# params[:father] = person1
+# params[:mother] = person2
+# params[:daughter] = person3
+# 
+# 
+# puts params[:father][:first]
+# puts params[:mother][:first]
+# puts params[:daughter][:first]
+# end
